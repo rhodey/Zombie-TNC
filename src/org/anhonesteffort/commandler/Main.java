419e938a7b3ca600abecb36fd67abdcbaeaef6e0
rhodey <rhodey@anhonesteffort.org>
2013-08-08 19:50:26 -0700

    fixed examples to show need for opening and configuring serial port.

--- src/org/anhonesteffort/commandler/Main.java
+++ src/org/anhonesteffort/commandler/Main.java
@@ -13,7 +13,8 @@ public class Main {
         try {
             // Open a serial port connected to a TNC in KISS mode.
             SerialPort serialPort = new SerialPort("COM9");
-
+            //serialPort.openPort();
+            //serialPort.setParams(SerialPort.BAUDRATE_9600, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
 
 
             /*
